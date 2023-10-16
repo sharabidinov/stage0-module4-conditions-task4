2c56fa87cd7207a166687141834c4f9712fcef74
sharabidinov <alymbeksharabidinov1@gmail.com>
2023-10-16 22:44:57 +0600

    solution

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max = (first > second && first > third) ? first : (second > first && second > third) ? second :
+                (third > first && third > second) ? third : first;
+
+        System.out.println(max);
     }
 }
